19ce39e852a1a0732376912e07c1aac5bf94662b
timplifier <timplifier@gmail.com>
2023-10-02 12:37:50 +0600

    feat(build.gradle.kts): added plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.agp.application) apply false
+    alias(libs.plugins.agp.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.protobuf) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.google.services) apply false
+}
+
+tasks.register(projectConfig.versions.tasks.clean.get(), Delete::class) {
+    delete(rootProject.buildDir)
 }
