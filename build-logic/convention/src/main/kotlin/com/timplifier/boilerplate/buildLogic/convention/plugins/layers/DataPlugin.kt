e2e23ee9f10d6ead8065fe5b92b49df961cfaded
timplifier <timplifier@gmail.com>
2023-10-02 18:27:32 +0600

    feat(dataplugin.kt): rearranged imports, deleted protobuf

--- build-logic/convention/src/main/kotlin/com/timplifier/boilerplate/buildLogic/convention/plugins/layers/DataPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/timplifier/boilerplate/buildLogic/convention/plugins/layers/DataPlugin.kt
@@ -1,18 +1,17 @@
 package com.timplifier.boilerplate.buildLogic.convention.plugins.layers
 
-import org.gradle.kotlin.dsl.dependencies
 import com.timplifier.boilerplate.buildLogic.convention.extensions.extractPluginId
 import com.timplifier.boilerplate.buildLogic.convention.extensions.implementation
 import com.timplifier.boilerplate.buildLogic.convention.extensions.ksp
 import com.timplifier.boilerplate.buildLogic.convention.extensions.libs
 import com.timplifier.boilerplate.buildLogic.convention.plugins.base.AndroidLibraryPlugin
+import org.gradle.kotlin.dsl.dependencies
 
 internal class DataPlugin : AndroidLibraryPlugin(projectConfiguration = {
 
     pluginManager.apply {
         apply(libs.plugins.kotlin.serialization.extractPluginId())
         apply(libs.plugins.google.devtools.ksp.extractPluginId())
-        apply(libs.plugins.google.protobuf.extractPluginId())
     }
 
     dependencies {
@@ -22,7 +21,6 @@ internal class DataPlugin : AndroidLibraryPlugin(projectConfiguration = {
         implementation(libs.bundles.kotlinx.core)
         implementation(libs.bundles.ktor.client)
         implementation(libs.androidx.paging)
-        implementation(libs.androidx.datastore)
         implementation(libs.google.dagger)
         ksp(libs.google.dagger.compiler)
     }
