1024af52d410b62bb14a28eee463ac5faf1dd782
timplifier <timplifier@gmail.com>
2023-10-02 15:02:41 +0600

    feat(core/domain): added domain layer

---
+++ core/domain/src/main/kotlin/com/timplifier/boilerplate/core/domain/Either.kt
@@ -0,0 +1,8 @@
+package com.timplifier.boilerplate.core.domain
+
+sealed class Either<out A, out B> {
+
+    class Left<out A>(val value: A) : Either<A, Nothing>()
+
+    class Right<out B>(val value: B) : Either<Nothing, B>()
+}
