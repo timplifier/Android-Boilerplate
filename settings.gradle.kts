0cf03700af341a8297422ab251831e27f9bdbc9b
timplifier <timplifier@gmail.com>
2023-10-02 12:34:30 +0600

    feat(settings.gradle.kts): enabled experimental features, created version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,22 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+
+    versionCatalogs {
+        create("androidProjectConfig") {
+            from(files("gradle/android-project-config.versions.toml"))
+        }
+        create("projectConfig") {
+            from(files("gradle/project-config.versions.toml"))
+        }
     }
 }
 
