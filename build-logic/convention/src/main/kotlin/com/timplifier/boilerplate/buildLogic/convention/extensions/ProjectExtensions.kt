b7cc88f9d3ebc849b2f486b61cf013934efaf8d2
timplifier <timplifier@gmail.com>
2023-10-02 14:40:00 +0600

    feat(convention/extensions/): added ProjectExtensions.kt file with extensions for `Project`

--- build-logic/convention/src/main/kotlin/com/timplifier/boilerplate/buildLogic/convention/extensions/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/com/timplifier/boilerplate/buildLogic/convention/extensions/ProjectExtensions.kt
@@ -6,7 +6,6 @@ import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
 import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory
 import org.gradle.api.plugins.ExtensionAware
-import org.gradle.kotlin.dsl.DependencyHandlerScope
 
 private inline fun <reified T> Project.retrieveExtension(name: String): T where T : AbstractExternalDependencyFactory {
     return (this as ExtensionAware).extensions.getByName(name) as T
@@ -21,31 +20,3 @@ internal inline val Project.androidProjectConfig: LibrariesForAndroidProjectConf
 
 internal inline val Project.projectConfig: LibrariesForProjectConfig
     inline get() = retrieveExtension("projectConfig")
-
-internal fun DependencyHandlerScope.implementation(dependencyNotation: Any) {
-    "implementation"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.testImplementation(dependencyNotation: Any) {
-    "testImplementation"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.androidTestImplementation(dependencyNotation: Any) {
-    "androidTestImplementation"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.debugImplementation(dependencyNotation: Any) {
-    "debugImplementation"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.api(dependencyNotation: Any) {
-    "api"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.ksp(dependencyNotation: Any) {
-    "ksp"(dependencyNotation)
-}
-
-internal fun DependencyHandlerScope.coreLibraryDesugaring(dependencyNotation: Any) {
-    "coreLibraryDesugaring"(dependencyNotation)
-}
